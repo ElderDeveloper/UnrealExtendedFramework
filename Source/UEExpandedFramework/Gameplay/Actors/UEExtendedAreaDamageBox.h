8b849b607a7ab826cb5259b1bcc330420cdd8d61
Kemal Erdem YILMAZ <kemalerdem09@gmail.com>
2021-11-04 16:09:59 +0300

    Damage

---
+++ Source/UEExpandedFramework/Gameplay/Actors/UEExtendedAreaDamageBox.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UEExtendedAreaDamage.h"
+#include "UEExtendedAreaDamageBox.generated.h"
+
+UCLASS()
+class UEEXPANDEDFRAMEWORK_API AUEExtendedAreaDamageBox : public AUEExtendedAreaDamage
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AUEExtendedAreaDamageBox();
+
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* BoxComponent;
+
+
+	UFUNCTION(BlueprintCallable)
+	void InitializeDamage(float damageAmount , FVector damageExtend , bool isLooping , float loopTime , float lifetime , TSubclassOf<UDamageType> damageType = nullptr);
+	
+	FVector DamageExtend;
+	
+	UFUNCTION()
+	void ApplyAreaDamage();
+};
---
+++ Source/UEExpandedFramework/Gameplay/Actors/UEExtendedAreaDamageSphere.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UEExtendedAreaDamageSphere.h"
+
+#include "NiagaraFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+AUEExtendedAreaDamageSphere::AUEExtendedAreaDamageSphere()
+{
+
+}
+
+void AUEExtendedAreaDamageSphere::InitializeDamage(float damageAmount, float radius, bool isLooping, float loopTime,float lifetime, TSubclassOf<UDamageType> damageType)
+{
+	if (GetOwner())
+	{
+		SetLifeSpan(lifetime + 0.1);
+		DamageSphereRadius = radius;
+		DamageAmount = damageAmount;
+		DamageType = damageType;
+		Lifetime = lifetime;
+	
+		if (isLooping)
+		{
+			SpawnEffects(DamageSoundStart,DamageEffectStart,DamageNiagaraStart);
+			GetWorldTimerManager().SetTimer(LoopHandle,this,&AUEExtendedAreaDamageSphere::ApplyAreaDamage,loopTime,isLooping);
+			ApplyAreaDamage();
+		}
+		else
+		{
+			SpawnEffects(DamageSoundStart,DamageEffectStart,DamageNiagaraStart);
+			ApplyAreaDamage();
+			Destroy();
+		}
+		
+	}
+	else
+	{
+		Destroy();
+	}
+
+}
+
+void AUEExtendedAreaDamageSphere::ApplyAreaDamage()
+{
+	TArray<AActor*> Ignore;
+	Ignore.Add(GetOwner());
+	UGameplayStatics::ApplyRadialDamage(GetWorld(),DamageAmount,GetActorLocation(),DamageSphereRadius,DamageType,Ignore,GetOwner(),GetOwner()->GetInstigatorController());
+	SpawnEffects(DamageSoundLoop,DamageEffectLoop,DamageNiagaraLoop);
+}
+
+
+
+
+
