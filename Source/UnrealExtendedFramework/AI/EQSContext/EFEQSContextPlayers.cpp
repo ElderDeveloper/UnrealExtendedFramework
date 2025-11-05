1f50ed60a10db4da0ef0f1cabb4e000517393cfa
Kemal Erdem YILMAZ <10424114+ElderDeveloper@users.noreply.github.com>
2025-11-05 21:15:03 +0300

    Context Players

---
+++ Source/UnrealExtendedFramework/AI/EQSContext/EFEQSContextPlayers.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EFEQSContextPlayers.h"
+
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "GameFramework/GameStateBase.h"
+#include "GameFramework/PlayerState.h"
+
+
+void UEFEQSContextPlayers::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+	Super::ProvideContext(QueryInstance, ContextData);
+
+	UWorld* World = QueryInstance.World;
+	if (!World)
+	{
+		return;
+	}
+	AGameStateBase* GameState = World->GetGameState();
+	if (!GameState)
+	{
+		return;
+	}
+
+	TArray<AActor*> Players;
+	for (APlayerState* PlayerState : GameState->PlayerArray)
+	{
+		if (PlayerState)
+		{
+			if (APawn* PlayerPawn = PlayerState->GetPawn())
+			{
+				Players.Add(PlayerPawn);
+			}
+		}
+	}
+	UEnvQueryItemType_Actor::SetContextHelper(ContextData, Players);
+	
+}
+
---
+++ Source/UnrealExtendedFramework/AI/EQSContext/EFEQSContextPlayers.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "EFEQSContextPlayers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALEXTENDEDFRAMEWORK_API UEFEQSContextPlayers : public UEnvQueryContext
+{
+	GENERATED_BODY()
+
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+
+	
+};
